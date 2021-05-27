d32d35cf3f40850e7249523cc6a7b72b7fedd9f6
Pierre-Yves <pybomt@gmail.com>
2021-05-27 08:28:37 +0200

    Création front-end Affichage des données rentrées via les boutons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  display: string = '0';
+
+  press(x) {
+    if( x==1 || x==2 || x==3 || x==4 || x==5 || x==6 || x==7 || x==8 || x==9 || x==0){
+      console.log(x, "nbr");
+      if(this.display !== '0'){
+        var displayNumber: number = +this.display;
+        displayNumber = displayNumber + x;
+        this.display = displayNumber.toString();
+      }
+      else{
+        var displayNumber: number = x;
+        this.display = displayNumber.toString();
+      }
+    }
+    else if(x='c'){
+      this.display = '0';
+    }
+
+  }
 
 }
